5ccdef634d1f0809a29b8fb4a106205b5c7e32cb
victor augusto <113953361+victorallves@users.noreply.github.com>
2022-11-09 16:29:22 -0300

    arrumando bugs, e melhorando o código

--- js/filtro.js
+++ js/filtro.js
@@ -1,49 +1,48 @@
 let campoBuscar = document.querySelector('#campo-busca');
 
+function limpaTabela(){
+  let tabelaAntiga = document.querySelectorAll('.usuario');
+
+  for(let i = 0; i < tabelaAntiga.length; i++)
+  {
+     tabelaAntiga[i].remove();
+  }
+}
+
 campoBuscar.addEventListener("input", function(){
 
   if(this.value.length > 0 ){
     let valorImput = this.value.toLowerCase();
 
-    let expNome = new RegExp (`\\D`, 'g').test(valorImput);
+    const texto = isNaN(+valorImput)?true:false
 
-    let expTelefone = new RegExp (`\\d`, 'g').test(valorImput);
-  
     let usuariosFiltrados;
     
-    if(expNome){
+    if(texto){
       usuariosFiltrados = pessoas.filter((pessoa) => 
       pessoa.nome.toLowerCase().startsWith(valorImput));
   
-      let tabelaAntiga = document.querySelectorAll('.usuario');
-
-      for(let i = 0; i < tabelaAntiga.length; i++)
-      {
-         tabelaAntiga[i].remove();
-      }
+     limpaTabela();
       
       criaTabela(usuariosFiltrados);
       
     }
     
-    if(expTelefone){
+    if(!texto){
       usuariosFiltrados = pessoas.filter((pessoa) => 
       pessoa.telefone.toString().startsWith(valorImput));
       
-      tabelaAntiga = document.querySelectorAll('.usuario');
-
-      for(let i = 0; i < tabelaAntiga.length; i++)
-      {
-         tabelaAntiga[i].remove();
-      }
-
+      limpaTabela();
+ 
       criaTabela(usuariosFiltrados);
         
     }
 
   }  
   else {
+    limpaTabela();
     criaTabela(pessoas);  
   }
 
 })
+
